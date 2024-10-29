2c2e0867834855a3fb47946b2e978e75528a752f
Janarthanan-Balasubramani <janarthananbalasubramani2024@gmail.com>
2024-10-30 04:09:47 +0530

    src-updated

--- src/index.js
+++ src/index.js
@@ -1,16 +1,13 @@
-import express, { json } from "express";
-import { PrismaClient } from "@prisma/client";
-import { start } from "./cron";
-import routes from "./routes";
+const express = require("express");
+const { PrismaClient } = require("@prisma/client");
+const routes = require("./routes");
 
 const app = express();
 const prisma = new PrismaClient();
 const PORT = process.env.PORT || 3000;
 
-app.use(json());
+app.use(express.json());
 app.use("/api/messages", routes);
-
-start(); // Start cron job
 
 app.listen(PORT, () =>
   console.log(`Server running on http://localhost:${PORT}`)
